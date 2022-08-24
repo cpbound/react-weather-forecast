8e88d0e378d7b81736100289ab41ce243ad761c7
Christian Bound <automaticbound@gmail.com>
2022-08-24 16:28:22 +0900

    search component done

---
+++ src/components/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'c612397afdmsh693abb7e50fcaddp13f3dfjsnfba162ea9371',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
