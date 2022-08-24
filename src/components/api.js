37f4ce2ef579525efeb389df4f2011096b3a7a27
Christian Bound <automaticbound@gmail.com>
2022-08-24 18:25:32 +0900

    current weather done. forecast to do

--- src/components/api.js
+++ src/components/api.js
@@ -8,5 +8,5 @@ export const geoApiOptions = {
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
 
-export const WEATHER_API_URL = "https://api.openweathermap.org/data/3.0"
-export const WEATHER_API_KEY = "4e4ab84b803ffb72dee8a951f186e8f8"
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "4e4ab84b803ffb72dee8a951f186e8f8";
